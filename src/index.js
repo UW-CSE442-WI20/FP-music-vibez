aa04385ab911754caa2aaa2b32878e7dd2af78c3
Aeron <aeronl2@cs.washington.edu>
2020-02-28 13:54:30 -0800

    added timeline slider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-
-
-// You can require libraries
+// libraries
 var d3 = require("d3");
 var cloud = require("d3-cloud");
+var slider = require("d3-simple-slider");
 
-// You can include local JS files:
+// local JS files:
 import MyClass from './my-class';
 const myClassInstance = new MyClass();
 myClassInstance.sayHi();
 
-
-// Anything you put in the static folder will be available
-// over the network, e.g.
 d3.csv('carbon-emissions.csv')
   .then((data) => {
     console.log('Dynamically loaded CSV data', data);
@@ -63,8 +59,47 @@ d3.select("body").append("svg")
             .duration(100)
             .style("font-size", function(d) { return d.size + "px"; });
     })
-    .on("click", function(d) {
-    alert(d.text);
+    .on("click", function(d) { 
+    alert(d.text); // instead we'll navigate to next page 
 });
 
 }
+
+// will want to use data from csv instead 
+var yearsData = []
+for (var i = 1946; i < 2020; i++) {
+	yearsData.push(i);
+}
+
+var yearsSelected = [1960, 1995]; 
+
+// Range Slider 
+var sliderRange = slider.sliderBottom()
+					.min(d3.min(yearsData))
+					.max(d3.max(yearsData))
+					.width(300)
+					.marks(yearsData)
+					.tickFormat(d3.format('.0f'))
+					.ticks(10)
+					.default(yearsSelected)
+					.fill('#045DE9')
+					.on('onchange', val => {
+					  d3.select('p#value-range').text(val.map(d3.format('.0f')).join(' - '));
+					  yearsSelected = val;
+					});
+
+var gRange = d3.select('div#slider-range')
+				.append('svg')
+				.attr('width', 500)
+				.attr('height', 100)
+				.append('g')
+				.attr('transform', 'translate(30,30)');
+
+gRange.call(sliderRange);
+
+d3.select('p#value-range').text(
+	sliderRange
+	  .value()
+	  .map(d3.format('.0f'))
+	  .join(' - ')
+);
