9da78fe30f5f4466eb9c0edb49645f25ba5266d1
Aeron <aeronl2@cs.washington.edu>
2020-03-02 15:21:15 -0800

    word cloud loads on page load

--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ d3.csv("carbon-emissions.csv").then(data => {
 });
 */
 
-var yearSelected = 2015; 
-
 var color = d3
   .scaleLinear()
   .domain([0, 1, 3, 5, 10, 15, 20, 50, 100])//[0, 1, 2, 3, 4, 5, 6, 10, 15, 20, 100])
@@ -34,7 +32,11 @@ var color = d3
   ]);
 
 
+var yearSelected = 2015; 
 var layout; 
+
+startWordMap();
+
 function startWordMap() {
 	layout = null;
 	layout = cloud()
@@ -123,7 +125,6 @@ var sliderStep = slider.sliderBottom()
 					.on('onchange', val => {
 					  d3.select('p#value-time').text(d3.format('.0f')(val));
 					  yearSelected = val;
-					  console.log(yearSelected);
 					  startWordMap();
 					});
 
@@ -134,44 +135,6 @@ var gStep = d3.select('div#slider-time')
 				.append('g')
 				.attr('transform', 'translate(30,30)');
 
-  gStep.call(sliderStep);
+gStep.call(sliderStep);
 
-  d3.select('p#value-time').text(d3.format('.0f')(sliderStep.value()));
-
-
-/*
-var yearsSelected = [1960, 1995];
-
-// Range Slider
-var sliderRange = slider
-  .sliderBottom()
-  .min(d3.min(yearsData))
-  .max(d3.max(yearsData))
-  .width(300)
-  .marks(yearsData)
-  .tickFormat(d3.format(".0f"))
-  .ticks(10)
-  .default(yearsSelected)
-  .fill("#045DE9")
-  .on("onchange", val => {
-    d3.select("p#value-range").text("Years " + val.map(d3.format(".0f")).join(" - "));
-    yearsSelected = val;
-  });
-
-var gRange = d3
-  .select("div#slider-range")
-  .append("svg")
-  .attr("width", 400)
-  .attr("height", 100)
-  .append("g")
-  .attr("transform", "translate(30,30)");
-
-gRange.call(sliderRange);
-
-d3.select("p#value-range").text("Years " + 
-  sliderRange
-    .value()
-    .map(d3.format(".0f"))
-    .join(" - ")
-);
-*/
+d3.select('p#value-time').text(d3.format('.0f')(sliderStep.value()));
