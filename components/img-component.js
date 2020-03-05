d2549b3559ac92b565a4184f05ca2ca9d017130e
Thomas Fuller <thomasfuller98@gmail.com>
2020-03-04 20:01:07 -0800

    Expand header

--- components/img-component.js
+++ components/img-component.js
@@ -1,25 +1,24 @@
 const React = require("react");
 
 class ImgComponent extends React.Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-  		super(props);
-  
-  		//this.changePropsColor = this.changePropsColor.bind(this);
-	}
+    //this.changePropsColor = this.changePropsColor.bind(this);
+  }
 
-	render() {
-		const { hasError, idyll, updateProps, ...props } = this.props;
-		//console.log(this.props.artistName);
-		//console.log(this.props.iconicImage);
+  render() {
+    const { hasError, idyll, updateProps, ...props } = this.props;
+    //console.log(this.props.artistName);
+    //console.log(this.props.iconicImage);
+    // <p>{this.props.artistName}</p>
 
-		return (
-		  <div {...props}>
-		  		<p>{this.props.artistName}</p>
-		      <img src={this.props.iconicImage} alt='Artist' class='img-button' />
-		  </div>
-		);
-	}
+    return (
+      <div {...props}>
+        <img src={this.props.iconicImage} alt="Artist" class="img-button" />
+      </div>
+    );
+  }
 }
 
 module.exports = ImgComponent;
