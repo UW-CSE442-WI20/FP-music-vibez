de05ebdf32b18596962e9419532bd02857dcd306
Aeron <aeronl2@cs.washington.edu>
2020-03-07 16:32:44 -0800

    initial buggy transitions on scroll

--- components/sales-chart.js
+++ components/sales-chart.js
@@ -2,11 +2,12 @@ const React = require("react");
 const D3Component = require("idyll-d3-component");
 const d3 = require("d3");
 
-const size = 600;
-const startYear = 10/30/1971;
-const endYear = 12/27/2003;
-var data = undefined; 
+var data = []; 
+var dots; 
 
+const margin = { top: 30, right: 40, bottom: 20, left: 50 };
+const width = 600;
+const height = 500;
 
 class SalesChart extends D3Component {
 
@@ -26,28 +27,20 @@ class SalesChart extends D3Component {
   }
 */
 
-  update(props) {
-    console.log(props.years);
-  }
-
   initialize(node, props) {
 
-      fetch(props.src)
+    fetch(props.src)
         .then((response) => {
         return response.text();
       }).then((text) => {
         data = d3.csvParse(text);
-
         data.forEach(function(d) {
           d.Year = Date.parse(d.Year);
         });
         // console.log(data);
-
-        const margin = { top: 30, right: 40, bottom: 20, left: 50 };
-        const width = 600;
-        const height = 500;
-
-
+       // const margin = { top: 30, right: 40, bottom: 20, left: 50 };
+       // const width = 600;
+       // const height = 500;
         this.svg = d3.select(node)
           .append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -73,7 +66,7 @@ class SalesChart extends D3Component {
           .call(d3.axisLeft(y));
 
         // Add dots
-        this.svg.append('g')
+        dots = this.svg.append('g')
           .selectAll("dot")
           .data(data)
           .enter()
@@ -82,10 +75,66 @@ class SalesChart extends D3Component {
             .attr("cy", function (d) { return y(d.Rank); } )
             .attr("r", 1.5)
             .style("fill", "#69b3a2");
+        return this.svg.node();
+      })
+  
+  }
+
+  // need to only add new points!! (or remove axes at least)
+  update(props) {
+    //console.log(props.years);
+    console.log("in update");
+      fetch(props.src)
+        .then((response) => {
+        return response.text();
+      }).then((text) => {
+        data = d3.csvParse(text);
+
+
+        var filterStart = Date.parse(props.years[0]);
+        var filterEnd = Date.parse(props.years[props.years.length - 1]);
+        console.log("start ",filterStart, props.years[0]);
+        console.log("end ", filterEnd);
+        var filteredData = [];
+        data.forEach(function(d) {
+          d.Year = Date.parse(d.Year);
+          if (filterStart <= d.Year && d.Year <= filterEnd) {
+            filteredData.push(d);
+          } 
+        });
+        //console.log(filteredData);
+
+        var x = d3.scaleLinear()
+          .domain([d3.min(filteredData, d => d.Year), d3.max(filteredData, d => d.Year)])
+          .range([ 0, width ]);
+        var y = d3.scaleLinear()
+          .domain([100, 1])
+          .range([ height, 0]); 
+
+        dots.data(filteredData).enter().append("circle")
+                        .attr("r",1.5);
+
+        dots.transition()
+            .duration(500)
+            .attr("cx", function (d) { return x(d.Year); } )
+            .attr("cy", function (d) { return y(d.Rank); } )
+            .attr("r", 1.5)
+            .style("fill", "#69b3a2");
+
+        dots.exit().remove();
+
+
+        //console.log(dots);
+
+       // dots.exit().remove();//remove unneeded circles
+       // dots.enter().append("circle")
+        //                .attr("r",1.5);
+
 
         return this.svg.node();
 
       })
+
 
   }
 
