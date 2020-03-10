230f6f84e8ca40cb5aa0e03c06f168186cc346cf
Aeron <aeronl2@cs.washington.edu>
2020-03-10 16:58:22 -0700

    michael's sales data

--- docs/static/idyll_index.js
+++ docs/static/idyll_index.js
@@ -244,6 +244,93 @@ var allData = {
     "album-name": "Chromatica",
     "release-date": "04/10/2020",
     "worldwide-sales": 0
+  }],
+
+  kanye: [{
+    "album-name": "The College Dropout",
+    "release-date": "02/10/2004",
+    "worldwide-sales": "4000000"
+  }, {
+    "album-name": "Late Registration",
+    "release-date": "08/30/2005",
+    "worldwide-sales": "3100000"
+  }, {
+    "album-name": "Graduation",
+    "release-date": "09/11/2007",
+    "worldwide-sales": "2700000"
+  }, {
+    "album-name": "808s & Heartbreaks",
+    "release-date": "11/24/2008",
+    "worldwide-sales": "1700000"
+  }, {
+    "album-name": "My Beautiful Dark Twisted Fantasy",
+    "release-date": "11/22/2010",
+    "worldwide-sales": "2000000"
+  }, {
+    "album-name": "Yeezus",
+    "release-date": "06/18/2013",
+    "worldwide-sales": "1000000"
+  }, {
+    "album-name": "The Life Of Pablo",
+    "release-date": "02/14/2016",
+    "worldwide-sales": "2000000"
+  }, {
+    "album-name": "Ye",
+    "release-date": "06/01/2018",
+    "worldwide-sales": "500000"
+  }, {
+    "album-name": "Jesus Is King",
+    "release-date": "10/25/2019",
+    "worldwide-sales": "300000"
+  }],
+  michael: [{
+    "album-name": "Got To Be There",
+    "release-date": "01/24/1972",
+    "worldwide-sales": null
+  }, {
+    "album-name": "Ben",
+    "release-date": "08/04/1972",
+    "worldwide-sales": null
+  }, {
+    "album-name": "Music & Me",
+    "release-date": "04/13/1973",
+    "worldwide-sales": null
+  }, {
+    "album-name": "Forever, Michael",
+    "release-date": "01/16/1975",
+    "worldwide-sales": null
+  }, {
+    "album-name": "Off the Wall",
+    "release-date": "08/10/1979",
+    "worldwide-sales": 20000000
+  }, {
+    "album-name": "Thriller",
+    "release-date": "11/30/1982",
+    "worldwide-sales": 66000000
+  }, {
+    "album-name": "Bad",
+    "release-date": "09/01/1987",
+    "worldwide-sales": 35000000
+  }, {
+    "album-name": "Dangerous",
+    "release-date": "11/26/1991",
+    "worldwide-sales": 32000000
+  }, {
+    "album-name": "HIStory: Past, Present and Future, Book I",
+    "release-date": "06/20/1995",
+    "worldwide-sales": 22000000
+  }, {
+    "album-name": "Invincible",
+    "release-date": "10/30/2001",
+    "worldwide-sales": 6000000
+  }, {
+    "album-name": "Michael",
+    "release-date": "06/25/2009",
+    "worldwide-sales": 541000
+  }, {
+    "album-name": "Xscape",
+    "release-date": "12/10/2010",
+    "worldwide-sales": 1700000
   }]
 };
 
